facb6c987ab70bee2c5f894c546906bcaf99c6d3
Lunar <gwkgxkgclydktflhxkrflgg@gmail.com>
2024-09-29 14:50:18 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -10,24 +10,45 @@ cls
 echo ##################################################
 echo ## MoonLib : https://github.com/Luunarr/moonlib ##
 echo ##################################################
-echo.
+
+cls
 git clone https://github.com/Luunarr/moonlib
-
+cls
+echo ##################################################
+echo ## MoonLib : https://github.com/Luunarr/moonlib ##
+echo ##################################################
 echo.
+echo [+] git clone https://github.com/Luunarr/moonlib
 
+cls
 cd moonlib
-
+cls
+echo ##################################################
+echo ## MoonLib : https://github.com/Luunarr/moonlib ##
+echo ##################################################
 echo.
+echo [+] git clone https://github.com/Luunarr/moonlib
+echo [+] cd moonlib
+
+cls
 pip install .
-
-
+cls
+echo ##################################################
+echo ## MoonLib : https://github.com/Luunarr/moonlib ##
+echo ##################################################
+echo.
 echo [+] git clone https://github.com/Luunarr/moonlib
 echo [+] cd moonlib
 echo [+] pip install moonlib
+cls
+
 echo.
 echo ##################################################
 echo ## MoonLib : https://github.com/Luunarr/moonlib ##
 echo ##################################################
+echo [+] git clone https://github.com/Luunarr/moonlib
+echo [+] cd moonlib
+echo [+] pip install moonlib
 echo >>> Python Import :
 echo from moonlib import *
 echo Cmoon, Smoon, Bmoon, Emoon
