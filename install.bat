514e81aeccb1e268331b8354efb0c32ed068f487
Lunar <gwkgxkgclydktflhxkrflgg@gmail.com>
2024-09-29 14:37:25 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,13 +4,9 @@
 :: MoonLib : https://github.com/Luunarr/moonlib ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::
 
-color 3
-
 echo ::::::::::::::::::::::::::::::::::::::::::::::::::
 echo :: MoonLib : https://github.com/Luunarr/moonlib ::
 echo ::::::::::::::::::::::::::::::::::::::::::::::::::
-
-color 2
 
 echo.
 echo [+] git clone https://github.com/Luunarr/moonlib
