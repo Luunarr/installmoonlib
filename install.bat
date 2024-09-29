4fcb641ca787b1ef11138f66fd3fd43d4e2d1723
Lunar <gwkgxkgclydktflhxkrflgg@gmail.com>
2024-09-29 14:33:32 +0200

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,17 @@
+@ehco off
+
+::::::::::::::::::::::::::::::::::::::::::::::::::
+:: MoonLib : https://github.com/Luunarr/moonlib ::
+::::::::::::::::::::::::::::::::::::::::::::::::::
+
+color 3
+cls
+echo [+] git clone https://github.com/Luunarr/moonlib
+git clone https://github.com/Luunarr/moonlib
+cls
+echo [+] cd moonlib
+cd moonlib
+cls
+echo [+] pip install moonlib
+pip install .
+pause
